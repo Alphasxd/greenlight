13fe0358f053f4f168db0a977dd31e162f18fe7f
Sheldon <alphasxd@outlook.com>
2023-08-14 21:24:47 +0800

    Update main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -42,14 +42,10 @@ func main() {
 		logger: logger,
 	}
 
-	// 定义一个路由器
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	// 定义一个http.Server实例
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
