e8b8dfdc3ee9afe8488b8fcd05845b728946c2fe
Sheldon <alphasxd@outlook.com>
2023-08-15 16:32:38 +0800

    perf: use writeJSON helper

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -13,20 +12,9 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version":     version,
 	}
 
-	// 将 map 转换为 JSON
-	// func json.Marshal(v any) ([]byte, error)
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request.", http.StatusInternalServerError)
-		return
 	}
-
-	// 添加换行，便于在终端中查看
-	js = append(js, '\n')
-
-	w.Header().Set("Content-Type", "application/json")
-
-	// 将包含 JSON 数据的字节切片写入响应体 
-	w.Write(js)
 }
