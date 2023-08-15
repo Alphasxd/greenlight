652d1aca0532e19f7dd11d6c8696b699a7a9cd47
Sheldon <alphasxd@outlook.com>
2023-08-15 17:19:38 +0800

    perf: use writeJSON let movie struct 2 json

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,21 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Alphasxd/greenlight/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "create a new movie")
+	fmt.Fprintln(w, "create a new movie")
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-    id, err := app.readIDParam(r)
-    if err != nil {
-        http.NotFound(w, r)
-        return
-    }
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
 
-	// 将 id 插入到响应中
-    fmt.Fprintf(w, "show the details of movie %d\n", id)
+	// 创建一个新的 Movie 实例
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	// 将 movie struct 实例封装为 JSON 格式并写入到响应体中
+    err = app.writeJSON(w, http.StatusOK, movie, nil)
+    if err != nil {
+        app.logger.Println(err)
+        http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+    }
 }
