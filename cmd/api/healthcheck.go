6ae213ce12ab9ff9212000a772e9529bc6071f67
Sheldon <alphasxd@outlook.com>
2023-08-15 16:15:35 +0800

    perf: json.Marshal let map2json

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// 创建 json 响应
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// 创建 map 用于存储当前应用的状态
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
 
-	// 将 json 响应写入到响应体中，并设置 Content-Type 头
+	// 将 map 转换为 JSON
+	// func json.Marshal(v any) ([]byte, error)
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request.", http.StatusInternalServerError)
+		return
+	}
+
+	// 添加换行，便于在终端中查看
+	js = append(js, '\n')
+
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+
+	// 将包含 JSON 数据的字节切片写入响应体 
+	w.Write(js)
 }
