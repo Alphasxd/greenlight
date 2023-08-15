d69ef04635c2f82cfdeeb95f88a17af640c282e7
Sheldon <alphasxd@outlook.com>
2023-08-15 16:25:01 +0800

    feat: write json helper

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -17,4 +18,27 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	// 将 data 封装成 JSON 格式
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// 添加换行，便于在终端中查看
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		// w.Header() 返回一个 map[string][]string 类型的 map
+		w.Header()[key] = value
+	}
+
+	// 设置响应头的 Content-Type 字段，并将状态码写入响应体和 JSON 数据
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
