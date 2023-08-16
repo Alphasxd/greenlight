d5c9b737bf1564ca0e68967a12de10cb8138d960
Sheldon <alphasxd@outlook.com>
2023-08-16 13:02:01 +0800

    create errors api

---
+++ cmd/api/errors.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// 记录错误日志
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
+
+// 向客户端发送 JSON 格式的错误信息和给定的状态码
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+// 当应用发生错误时，记录错误日志，并调用 errorResponse() 方法发送响应到客户端
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	msg := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, msg)
+}
+
+// 向客户端发送 404 错误响应和 JSON 格式 Response
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	msg := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, msg)
+}
+
+// 向客户端发送 405 错误响应和 JSON 格式 Response
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	msg := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, msg)
+}
