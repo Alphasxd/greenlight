ca4809807936e005addf170551b1d7a0fc6869ea
Sheldon <alphasxd@outlook.com>
2023-08-17 17:18:28 +0800

    perform the validation in createMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/Alphasxd/greenlight/internal/data"
+	"github.com/Alphasxd/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +22,34 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// 创建一个新的 Validator 实例
+	v := validator.New()
+
+	// 检查 Title 字段
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	// 检查 Year 字段
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	// 检查 Runtime 字段
+	v.Check(input.Runtime != 0, "runtime", "must be provided")
+	v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
+
+	// 检查 Genres 字段
+	v.Check(input.Genres != nil, "genres", "must be provided")
+	v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+
+	// 如果有任何错误，调用 failedValidationResponse() 方法发送响应
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
