1028ef6c8a55828db64527329338e2e3f983b74a
Sheldon <alphasxd@outlook.com>
2023-08-17 17:11:39 +0800

    use runtime replace int32

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,10 +10,10 @@ import (
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	// 初始化一个新的 json.Decoder 实例，将其绑定到 http.Request.Body
