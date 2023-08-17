47b714d047752db4ab8f9237e3aa7d61f6036eca
Sheldon <alphasxd@outlook.com>
2023-08-17 15:20:32 +0800

    restrict inputs

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -49,8 +50,17 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+
+	// 使用 http.MaxBytesReader() 限制读取请求体的大小为 1MB
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	// 初始化 json.Decoder，在解码之前调用 DisallowUnknownFields() 方法
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
 	// 将请求体中的 JSON 数据解码到目标变量 dst 中
-	err := json.NewDecoder(r.Body).Decode(dst)
+	err := dec.Decode(dst)
 	if err != nil {
 		// 定义可能出现的错误类型
 		var syntaxError *json.SyntaxError                     // 语法错误
@@ -59,22 +69,42 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 
 		// 根据错误类型，返回自定义的错误消息
 		switch {
+
 		case errors.As(err, &syntaxError):
 			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
 		case errors.Is(err, io.ErrUnexpectedEOF):
 			return errors.New("body contains badly-formed JSON")
+
 		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != "" {
 				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
 			}
 			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
+
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unkown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
+
 		default:
 			return err
 		}
 	}
+
+	// 再次调用 Decode() 方法，确保请求体只包含单个 JSON 值
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
 	return nil
 }
