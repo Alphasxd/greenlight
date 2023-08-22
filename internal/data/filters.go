cf3666ee10e82eae6e1b15a4077fa2336c1e3070
Sheldon <alphasxd@outlook.com>
2023-08-22 15:40:27 +0800

    validate query string param

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,7 +1,24 @@
 package data
 
+import "github.com/Alphasxd/greenlight/internal/validator"
+
 type Filters struct {
-	Page     int
-	PageSize int
-	Sort     string
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafelist []string
+}
+
+func ValidateFilters(v *validator.Validator, f Filters) {
+
+	// 检查 Page 字段
+	v.Check(f.Page > 0, "page", "must be greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
+
+	// 检查 PageSize 字段
+	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
+
+	// 检查 Sort 字段
+	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
