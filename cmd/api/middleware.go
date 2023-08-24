dfc81e82e32012e5e3e525b5aabd8310dc27a5a0
Sheldon <alphasxd@outlook.com>
2023-08-24 15:56:50 +0800

    create rateLimit middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -13,6 +15,20 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// 创建一个速率限制器，设置每秒允许的请求数量为 2，设置桶的容量为 4
+	limiter := rate.NewLimiter(2, 4)
+	// 返回一个闭包函数，这个函数包装了 limiter 对象的 Allow 方法的调用
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 如果请求超过了速率限制器的限制，则向客户端发送一个带有 429 状态码的响应
+		if !limiter.Allow() {
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
