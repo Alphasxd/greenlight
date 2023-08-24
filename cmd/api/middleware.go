dfd7b86559065ca87c952baaad437399f668ef94
Sheldon <alphasxd@outlook.com>
2023-08-24 20:59:37 +0800

    only carry out check if rate limiting is enabled

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -53,31 +53,36 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 使用 SplitHostPort() 函数从请求的远程地址中提取host部分，赋值给ip变量
-		ip, _, err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil {
-			app.serverErrorResponse(w, r, err)
-			return
-		}
+		// 只有当速率限制器是启用的时候，才会执行速率限制
+		if app.config.limiter.enabled {
+			// 使用 SplitHostPort() 函数从请求的远程地址中提取host部分，赋值给ip变量
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				app.serverErrorResponse(w, r, err)
+				return
+			}
 
-		mu.Lock()
+			mu.Lock()
 
-		// 为 IP 地址对应的客户端创建一个新的速率限制器，存储到 clients map 中
-		if _, found := clients[ip]; !found {
-			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
-		}
+			// 为 IP 地址对应的客户端创建一个新的速率限制器，存储到 clients map 中
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+				}
+			}
 
-		// 更新客户端的最后活跃时间
-		clients[ip].lastSeen = time.Now()
+			// 更新客户端的最后活跃时间
+			clients[ip].lastSeen = time.Now()
 
-		// 检查与当前请求关联的速率限制器是否允许这个请求，如果不允许，则返回一个带有 429 状态码的响应
-		if !clients[ip].limiter.Allow() {
+			// 检查与当前请求关联的速率限制器是否允许这个请求，如果不允许，则返回一个带有 429 状态码的响应
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+
 			mu.Unlock()
-			app.rateLimitExceededResponse(w, r)
-			return
 		}
-
-		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
