02fafe4618f51eaf6892d54725e150639f1f96f1
Sheldon <alphasxd@outlook.com>
2023-08-24 20:30:24 +0800

    ip-based rate limiting and regular cleaning old limiters

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -20,15 +23,62 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// 创建一个速率限制器，设置每秒允许的请求数量为 2，设置桶的容量为 4
-	limiter := rate.NewLimiter(2, 4)
-	// 返回一个闭包函数，这个函数包装了 limiter 对象的 Allow 方法的调用
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	// 声明一个互斥锁和一个map，用来存储IP地址和相应的速率限制器
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// 启动一个后台goroutine，定期清理map中过期的速率限制器
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+
+			for ip, client := range clients {
+				// 如果速率限制器距离上一次使用超过了 3 分钟，则将其从 map 中删除
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			mu.Unlock()
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 如果请求超过了速率限制器的限制，则向客户端发送一个带有 429 状态码的响应
-		if !limiter.Allow() {
+		// 使用 SplitHostPort() 函数从请求的远程地址中提取host部分，赋值给ip变量
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		mu.Lock()
+
+		// 为 IP 地址对应的客户端创建一个新的速率限制器，存储到 clients map 中
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+
+		// 更新客户端的最后活跃时间
+		clients[ip].lastSeen = time.Now()
+
+		// 检查与当前请求关联的速率限制器是否允许这个请求，如果不允许，则返回一个带有 429 状态码的响应
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		mu.Unlock()
+
 		next.ServeHTTP(w, r)
 	})
 }
