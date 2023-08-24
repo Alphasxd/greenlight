2e371b39cfac217e5ae2945381ddb14f9ba0a86b
Sheldon <alphasxd@outlook.com>
2023-08-24 11:21:29 +0800

    use new Logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,12 +5,12 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/Alphasxd/greenlight/internal/data"
+	"github.com/Alphasxd/greenlight/internal/jsonlog"
 
 	_ "github.com/lib/pq"
 )
@@ -33,7 +33,7 @@ type config struct {
 // 应用结构体，用于存储应用程序的依赖项，handler，helper，middleware，logger等
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -53,22 +53,22 @@ func main() {
 	flag.Parse()
 
 	// 初始化一个logger实例
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 
 	// 延迟关闭数据库连接
 	defer func(db *sql.DB) {
 		err := db.Close()
 		if err != nil {
-			logger.Fatal(err)
+			logger.PrintFatal(err, nil)
 		}
 	}(db)
 
-	logger.Printf("database connection pool established")
+	logger.PrintInfo("database connection pool established", nil)
 
 	// 初始化一个application实例
 	app := &application{
@@ -87,9 +87,12 @@ func main() {
 	}
 
 	// 启动 http server
-	logger.Printf("Starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("Starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  cfg.env,
+	})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
