ecf1a0b8c2706ee8ad9c6c30234e33c10559f526
Sheldon <alphasxd@outlook.com>
2023-08-25 16:00:25 +0800

    shutdown gracefully

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -18,6 +20,9 @@ func (app *application) serve() error {
 		WriteTimeout: 30 * time.Second,
 	}
 
+	// shutdownError 通道用来接收服务器关闭时返回的错误
+	shutdownError := make(chan error)
+
 	// 启动一个goroutine来监听操作系统的中断信号
 	go func() {
 		quit := make(chan os.Signal, 1)
@@ -29,11 +34,17 @@ func (app *application) serve() error {
 		s := <-quit
 
 		// 记录信号的名称，使用 String() 方法将信号转换为字符串
-		app.logger.PrintInfo("caught signal", map[string]string{
+		app.logger.PrintInfo("shutting down server", map[string]string{
 			"signal": s.String(),
 		})
 
-		os.Exit(0)
+		// 创建一个5秒超时的上下文
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		// 调用 Shutdown() 方法来关闭服务器，传入我们自己创建的上下文
+		// 如果成功，Shutdown() 方法会返回 nil，如果失败，说明没有在 5 秒内完成所有活动的连接，
+		shutdownError <- srv.Shutdown(ctx)
 	}()
 
 	app.logger.PrintInfo("starting server", map[string]string{
@@ -41,5 +52,22 @@ func (app *application) serve() error {
 		"env":  app.config.env,
 	})
 
-	return srv.ListenAndServe()
+	// 检查 err 是否为 http.ErrServerClosed，如果不是，则返回 err
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	// 等待接受 shutdownError 通道返回的错误
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	// 服务器已经优雅的关闭，记录日志并退出
+	app.logger.PrintInfo("stopped server", map[string]string{
+		"addr": srv.Addr,
+	})
+
+	return nil
 }
