0ed240018b4a65e47f9f5937700901fcd644b5b0
Sheldon <alphasxd@outlook.com>
2023-08-29 15:29:34 +0800

    setting up the UserModel

--- internal/data/models.go
+++ internal/data/models.go
@@ -15,11 +15,13 @@ var (
 // Models 定义一个模型结构体，包含所有模型的实例
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 // NewModels 函数返回一个包含所有模型的 Models 结构体实例
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
---
+++ internal/data/users.go
@@ -0,0 +1,187 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/Alphasxd/greenlight/internal/validator"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrDuplicateEmail = errors.New("duplicate email")
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+// Set 方法用于将明文密码 plaintextPassword 转换为哈希值，并将其保存在 p.hash 字段中。
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+// Matches 方法用于检查明文密码 plaintextPassword 是否与哈希值 p.hash 匹配。
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+
+		}
+	}
+	return true, nil
+}
+
+// ValidateEmail 方法检查电子邮件地址是否有效，并将错误消息添加到 v.Errors 中。
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+// ValidatePasswordPlaintext 方法检查密码是否有效，并将错误消息添加到 v.Errors 中。
+func ValidatePasswordPlaintext(v *validator.Validator, passwd string) {
+	v.Check(passwd != "", "password", "must be provided")
+	v.Check(len(passwd) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(passwd) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+// ValidateUser 方法检查用户结构体中的值是否有效。如果有错误，方法会将错误添加到 v.Errors 中。
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+	ValidateEmail(v, user.Email)
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+	// 如果密码哈希值为空，则说明密码未设置，直接panic，不用添加到v.Errors中
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
+}
+
+// Insert 方法将一个新用户添加到 users 数据表中。
+func (m UserModel) Insert(user *User) error {
+	query := `
+		INSERT INTO users (name, email, password_hash, activated) 
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, created_at, version`
+	args := []any{
+		user.Name,
+		user.Email,
+		user.Password.hash,
+		user.Activated,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// user.ID, user.CreatedAt, user.Version 会被赋值
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
+	if err != nil {
+		switch {
+		// 如果电子邮件地址已经被使用，那么返回 ErrDuplicateEmail 错误
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+// GetByEmail 方法返回与指定电子邮件地址匹配的用户记录。
+func (m UserModel) GetByEmail(email string) (*User, error) {
+	query := `
+		SELECT id, created_at, name, email, password_hash, activated, version
+        FROM users
+        WHERE email = $1`
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &user, nil
+}
+
+// Update 方法用于更新现有用户记录。
+func (m UserModel) Update(user *User) error {
+	query := `
+		UPDATE users 
+        SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1
+        WHERE id = $5 AND version = $6
+        RETURNING version`
+
+	args := []any{
+		user.Name,
+		user.Email,
+		user.Password.hash,
+		user.Activated,
+		user.ID,
+		user.Version,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// 通过 Version 字段实现乐观并发控制
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
+}
