cad83a9b4b454f58f6a8651fa08f0c5c82500b45
Sheldon <alphasxd@outlook.com>
2023-08-30 11:33:53 +0800

    create secure activation tokens

--- internal/data/models.go
+++ internal/data/models.go
@@ -15,6 +15,7 @@ var (
 // Models 定义一个模型结构体，包含所有模型的实例
 type Models struct {
 	Movies MovieModel
+	Tokens TokenModel
 	Users  UserModel
 }
 
@@ -22,6 +23,7 @@ type Models struct {
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Tokens: TokenModel{DB: db},
 		Users:  UserModel{DB: db},
 	}
 }
