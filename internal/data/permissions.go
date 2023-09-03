0e6c4991c9ebd61c94f9f10a0050264e422c4a5a
Sheldon <alphasxd@outlook.com>
2023-09-03 19:05:19 +0800

    grant default permission to new user

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 // Permissions 定义了用户可以拥有的权限列表
@@ -64,4 +66,17 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+// AddForUser 将指定的权限代码添加到指定用户的权限列表中
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+        INSERT INTO users_permissions
+        SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
